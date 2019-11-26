66925bd98c6c0052828fbf529811d830731f0f5b
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-11-26 13:25:02 -0500

    Final

---
+++ src/test.js
@@ -0,0 +1,50 @@
+const readlineSync = require("readline-sync");
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+
+let origin = "";
+let destination = "";
+let temp = null;
+let temp2;
+
+console.log()
+
+while (origin !== "c" && origin !== "f" && origin !== "k") {
+  origin = (readlineSync.question("Enter an origin scale: ")).toLowerCase();
+}
+
+while (destination !== "c" && destination !== "f" && destination !== "k") {
+  destination = (readlineSync.question("Enter a destination scale: ")).toLowerCase();
+}
+
+while (temp > MAX || temp < MIN || temp === null) {
+  temp = (readlineSync.question("Enter a temperature: "));
+  temp2 = (temp).toLocaleString("en", { minimumFractionDigits: 0, maximumFractionDigits: 1})
+}
+
+if (origin === "c"){
+  if (destination = "f"){
+temp = (temp * (9 / 5)) + 32
+console.log(temp2 + "\u00b0C equals" + temp + "\u00b0F")
+  } else if (destination = "k"){
+temp = temp + 273.15
+console.log(temp2 + "\u00b0C equals" + temp + "\u00b0K")
+  }
+} else if (origin === "f"){
+  if (destination = "c"){
+temp = (temp - 32) * (5 / 9)
+console.log(temp2 + "\u00b0F equals" + temp + "\u00b0C")
+  } else if (destination = "k"){
+temp = (temp - 32) * (5 / 9) + 273.15
+console.log(temp2 + "\u00b0F equals" + temp + "\u00b0K")
+} else if (origin === "k"){
+    if (destination = "c"){
+  temp = temp - 273.15
+  console.log(temp2 + "\u00b0K equals" + temp + "\u00b0C")
+} else if (destination = "f"){
+  temp = (temp - 273.15) * (5 / 9) + 32
+  console.log(temp2 + "\u00b0K equals" + temp + "\u00b0Fss")
+    }
+  }
+}
